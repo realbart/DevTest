87e59c19fd466e7f64dca23fcc8e14165c709e92
Bart Kemps ☺ <b.kemps@rdc.nl>
2023-04-11 02:19:23 +0200

    remove irrelevant comments

--- Refactoring/CommandExecution/Commands/CreateRhombusCommand.cs
+++ Refactoring/CommandExecution/Commands/CreateRhombusCommand.cs
@@ -21,11 +21,7 @@ public class CreateRhombusCommand : ICommand
     /// </summary>
     /// <param name="context">The command context containing all state.</param>
     /// <param name="height">The distance from the base to the highest point.</param>
-    /// <param name="baseLength">The side that is perpendicular to the height of a triangle.</param>
-    /// <remarks>
-    /// Strictly speaking, the width of a shape is traditionally interpreted to mean the width
-    /// of the bounding box, yielding different results for obtuse-angled triangles.
-    /// </remarks>
+    /// <param name="baseLength">The side that is perpendicular to the height of a rhombus.</param>
     private static void Invoke(CommandContext context, double height, double baseLength)
     {
         var surfaceArea = height * baseLength;
